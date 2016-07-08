d885ae4afa4c5496a18db9ef63b68b9525e024ce
quang <tdquang7@gmail.com>
2016-07-08 14:32:22 +0700

    Xong chuyển đổi giao diện

---
+++ straw/straw/ContractorController.swift
@@ -0,0 +1,13 @@
+//
+//  ContractorController.swift
+//  straw
+//
+//  Created by quang on 7/8/16.
+//  Copyright © 2016 slifer7. All rights reserved.
+//
+
+import UIKit
+
+class ContractorController: UITableViewController {
+    
+}
